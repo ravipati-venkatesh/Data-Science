0199001af132d9ac15bb1047698aa5a3ad92bfb5
ravipati venkatesh <ravipati1347.rv@gmail.com>
2024-06-27 19:42:03 +0530

    Added required changes to do avoid negative values in vectors for multinomial NB

--- Projects/SentimentAnalysis/src/Main.py
+++ Projects/SentimentAnalysis/src/Main.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from multiprocessing import Pool, cpu_count
 import time
+from sklearn.preprocessing import MinMaxScaler
 
 # Add the src paths to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "Model")))
@@ -37,50 +38,58 @@ label_encoder = LabelEncoder()
 df['sentiment_encoded'] = label_encoder.fit_transform(df['sentiment'])
 
 vectorizer_dict = [
-#     {'vectorizer': 'count', 'review_column': 'cleaned_review', 'pretrained': None},
-#     {'vectorizer': 'tf-idf', 'review_column': 'cleaned_review', 'pretrained': None},
-#     {'vectorizer': 'word2vec', 'review_column': 'cleaned_review', 'pretrained': False},
-#     {'vectorizer': 'fasttext', 'review_column': 'cleaned_review', 'pretrained': False},
+    {'vectorizer': 'count', 'review_column': 'cleaned_review', 'pretrained': None},
+    {'vectorizer': 'tf-idf', 'review_column': 'cleaned_review', 'pretrained': None},
+    {'vectorizer': 'word2vec', 'review_column': 'cleaned_review', 'pretrained': False},
+    {'vectorizer': 'fasttext', 'review_column': 'cleaned_review', 'pretrained': False},
 #     {'vectorizer': 'word2vec', 'review_column': 'cleaned_review', 'pretrained': True},
 #     {'vectorizer': 'fasttext', 'review_column': 'cleaned_review', 'pretrained': True},
 #     {'vectorizer': 'word2vec', 'review_column': 'review', 'pretrained': False},
 #     {'vectorizer': 'fasttext', 'review_column': 'review', 'pretrained': False},
-    {'vectorizer': 'word2vec', 'review_column': 'review', 'pretrained': True},
-    {'vectorizer': 'fasttext', 'review_column': 'review', 'pretrained': True},
+    # {'vectorizer': 'word2vec', 'review_column': 'review', 'pretrained': True},
+    # {'vectorizer': 'fasttext', 'review_column': 'review', 'pretrained': True},
 ]
 
-models = {
-    'Logistic Regression',
-#     'Multinomial Naive Bayes',
-#     'Decision Tree',
-#     'Random Forest',
+
+# Run one model at a time
+models = list({
+    # 'Logistic Regression',
+    # 'Multinomial Naive Bayes',
+    'Decision Tree',
+    # 'Random Forest',
 #     'SVC',
 #     'KNN'
-}
+})
 
 # Function to process a single combination of vectorizer and model
 def process_combination(params):
     dic, model_name, df = params
+    t = time.time()
     df_ = dp.vectorization(df, vectorizer=dic['vectorizer'], review_column=dic['review_column'], pretrained=dic['pretrained'])
     X_train, X_test, y_train, y_test = Models.train_test_data_split(df_)
+
+    if model_name=='Multinomial Naive Bayes' and (dic['vectorizer']=='word2vec' or dic['vectorizer']=='fasttext'):
+      # Apply MinMaxScaler to transform the feature vectors to be non-negative
+      scaler = MinMaxScaler()
+      X_train = scaler.fit_transform(X_train)
+      X_test = scaler.transform(X_test)
     Models.fit_models(X_train, y_train, model_name, dic['vectorizer'])
     accuracy, precision, recall, f1 = Models.predict_models_summary(X_test, y_test, model_name, dic['vectorizer'])
-    return dic['vectorizer'], model_name, accuracy, precision, recall, f1
+    return dic['vectorizer'], model_name, accuracy, precision, recall, f1, time.time() - t
 
 # Prepare parameters for multiprocessing
 params = [(dic, model_name, df) for dic in vectorizer_dict for model_name in models]
 
 # Use multiprocessing to process combinations
 if __name__ == '__main__':
-    t = time.time()
+    print(cpu_count())
     with Pool(cpu_count() - 1) as pool:
         results = pool.map(process_combination, params)
 
     # Create summary DataFrame
-    df_summary = pd.DataFrame(results, columns=['vectorizer', 'model_name', 'accuracy', 'precision', 'recall', 'f1'])
+    df_summary = pd.DataFrame(results, columns=['vectorizer', 'model_name', 'accuracy', 'precision', 'recall', 'f1', 'time_taken'])
 
-    print("Done in:", time.time() - t)
-    summary_file_path = "../Data/Process/{models[0].replace(" ", "_")}.xlsx"
+    summary_file_path = f'../Data/Process/{models[0].replace(" ", "_")}.xlsx'
 #     print(df_summary)
     df_summary.to_excel(summary_file_path, index=False)
 
