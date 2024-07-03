2f232412dd1c9841c74eb89f64931ea735dc5aca
ravipati venkatesh <ravipati1347.rv@gmail.com>
2024-07-03 18:51:00 +0530

    Added convolutional layer on top of LSTM

--- Projects/SentimentAnalysis/src/Deep_Learning_main.py
+++ Projects/SentimentAnalysis/src/Deep_Learning_main.py
@@ -7,17 +7,21 @@
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, GRU, RNN, Dense, BatchNormalization, SimpleRNN
+from tensorflow.keras.layers import Embedding, LSTM, GRU, RNN, Dense, BatchNormalization, SimpleRNN, Dropout
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
+from keras.utils import to_categorical
 
 def preprocess_data(df):
-    label_encoder = LabelEncoder()
-    df['sentiment_encoded'] = label_encoder.fit_transform(df['sentiment'])
-    X_train, X_test, y_train, y_test = train_test_split(df['review'], df['sentiment_encoded'], test_size=0.2, random_state=42)
+    # label_encoder = LabelEncoder()
+    # df['sentiment_encoded'] = label_encoder.fit_transform(df['sentiment'])
+        # Convert sentiment labels to categorical encoding
+    df['sentiment'] = pd.Categorical(df['sentiment'])
+    df['sentiment_encoded'] = df['sentiment'].cat.codes
+    y = to_categorical(df['sentiment_encoded'])
+    X_train, X_test, y_train, y_test = train_test_split(df['review'], y, test_size=0.2, random_state=42)
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(X_train.values)
     X_train_seq = tokenizer.texts_to_sequences(X_train.values)
@@ -27,7 +31,7 @@ def preprocess_data(df):
     X_train_pad = pad_sequences(X_train_seq, maxlen=maxlen)
     X_test_pad = pad_sequences(X_test_seq, maxlen=maxlen)
     vocab_size = max([item for sublist in X_train_seq for item in sublist])
-    return X_train_pad, X_test_pad, y_train, y_test, label_encoder, vocab_size
+    return X_train_pad, X_test_pad, y_train, y_test, vocab_size
 
 
 def model_def_and_compile(model_name, vocab_size):
@@ -38,15 +42,21 @@ def model_def_and_compile(model_name, vocab_size):
     model.add(BatchNormalization())
     
     if model_name == "LSTM":
-        model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2, return_sequences=False))
+        model.add(Conv1D(filters=64, kernel_size=5, activation='relu'))
+        model.add(MaxPooling1D(pool_size=4))
+        model.add(LSTM(100, dropout=0.3, recurrent_dropout=0.3, return_sequences=False))
     elif model_name == "GRU":
-        model.add(GRU(100, dropout=0.2, recurrent_dropout=0.2, return_sequences=False))
+        model.add(Conv1D(filters=64, kernel_size=5, activation='relu'))
+        model.add(MaxPooling1D(pool_size=4))
+        model.add(GRU(100, dropout=0.3, recurrent_dropout=0.3, return_sequences=False))
     elif model_name == "RNN":
-        model.add(SimpleRNN(100, dropout=0.2, recurrent_dropout=0.2, return_sequences=False))
+        model.add(Conv1D(filters=64, kernel_size=5, activation='relu'))
+        model.add(MaxPooling1D(pool_size=4))
+        model.add(SimpleRNN(100, dropout=0.3, recurrent_dropout=0.3, return_sequences=False))
         
     model.add(BatchNormalization())
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dropout(0.4))
+    model.add(Dense(2, activation='softmax'))
     
     # Compile the model
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -74,7 +84,7 @@ if __name__ == '__main__':
     
     # Load preprocessed data
     df = pd.read_excel(processed_file_path)
-    X_train_pad, X_test_pad, y_train, y_test, label_encoder, vocab_size = preprocess_data(df)
+    X_train_pad, X_test_pad, y_train, y_test, vocab_size = preprocess_data(df)
     
     results = []
     for model_name in model_names:
